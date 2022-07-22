25fdd605ea830b4820712f96a930e1cb1104c8e7
Scott Graham <scttgrhm7+public@gmail.com>
2022-07-21 18:45:34 -0700

    add region cards
    Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com>
    Co-authored-by: Miles Abbason <miles.abbason@gmail.com>
    Co-authored-by: Tim Dronkers <tdronkers@gmail.com>

--- src/components/test-new/LocationsInput.jsx
+++ src/components/test-new/LocationsInput.jsx
@@ -1,9 +1,20 @@
 import { React } from 'react';
+import { useSelector } from 'react-redux';
+import RegionCard from './RegionCard';
 
 function LocationsInput() {
-  return (
-    <div>Locations!</div>
-  );
+  const regions = useSelector((state) => state.sideloads.regions);
+
+  if (regions) {
+    return (
+      <div>
+        <h2>Locations</h2>
+        <div className="grid grid-cols-4 gap-4">
+          { regions.map((region) => <RegionCard key={region.id} region={region} />)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LocationsInput;
