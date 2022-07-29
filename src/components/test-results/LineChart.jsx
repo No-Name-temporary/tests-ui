11c33dc656d21f9e049386879616cc589e85e5e2
Scott Graham <scttgrhm7+public@gmail.com>
2022-07-29 13:51:43 -0700

    add code for multiline graph
    Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com>
    Co-authored-by: Miles Abbason <miles.abbason@gmail.com>
    Co-authored-by: Tim Dronkers <tdronkers@gmail.com>

--- src/components/test-results/LineChart.jsx
+++ src/components/test-results/LineChart.jsx
@@ -2,62 +2,178 @@ import React, { useEffect } from 'react';
 import * as d3 from 'd3';
 
 function LineChart({ testRuns }) {
-  const createGraph = async () => {
-    const data = [];
-    testRuns.forEach((run) => {
-      const runDataForChart = {
-        region: run.region,
-        date: new Date(run.completedAt),
-        value: run.responseTime,
-      };
-      data.push(runDataForChart);
-    });
-    console.log(data);
+  console.log(testRuns);
+  function createGraph(data, {
+    x = ([x]) => x, // given d in data, returns the (temporal) x-value
+    y = ([, y]) => y, // given d in data, returns the (quantitative) y-value
+    z = () => 1, // given d in data, returns the (categorical) z-value
+    title, // given d in data, returns the title text
+    defined, // for gaps in data
+    curve = d3.curveLinear, // method of interpolation between points
+    marginTop = 20, // top margin, in pixels
+    marginRight = 30, // right margin, in pixels
+    marginBottom = 30, // bottom margin, in pixels
+    marginLeft = 40, // left margin, in pixels
+    width = 640, // outer width, in pixels
+    height = 400, // outer height, in pixels
+    xType = d3.scaleUtc, // type of x-scale
+    xDomain, // [xmin, xmax]
+    xRange = [marginLeft, width - marginRight], // [left, right]
+    yType = d3.scaleLinear, // type of y-scale
+    yDomain, // [ymin, ymax]
+    yRange = [height - marginBottom, marginTop], // [bottom, top]
+    yFormat, // a format specifier string for the y-axis
+    yLabel, // a label for the y-axis
+    zDomain, // array of z-values
+    color = 'currentColor', // stroke color of line, as a constant or a function of *z*
+    strokeLinecap, // stroke line cap of line
+    strokeLinejoin, // stroke line join of line
+    strokeWidth = 1.5, // stroke width of line
+    strokeOpacity, // stroke opacity of line
+    mixBlendMode = 'multiply', // blend mode of lines
+    voronoi, // show a Voronoi overlay? (for debugging)
+  } = {}) {
+    // Compute values.
+    const X = d3.map(data, x);
+    const Y = d3.map(data, y);
+    const Z = d3.map(data, z);
+    const O = d3.map(data, (d) => d);
+    if (defined === undefined) defined = (d, i) => !isNaN(X[i]) && !isNaN(Y[i]);
+    const D = d3.map(data, defined);
 
-    // set the dimensions and margins of the graph
-    const margin = {
-      top: 20, right: 20, bottom: 50, left: 70,
-    };
-    const width = 960 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
-    // append the svg object to the body of the page
-    const svg = d3.select('#chart').append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', `translate(${margin.left},     ${margin.top})`);
+    // Compute default domains, and unique the z-domain.
+    if (xDomain === undefined) xDomain = d3.extent(X);
+    if (yDomain === undefined) yDomain = [0, d3.max(Y, (d) => (typeof d === 'string' ? +d : d))];
+    if (zDomain === undefined) zDomain = Z;
+    zDomain = new d3.InternSet(zDomain);
 
-    // Add X axis and Y axis
-    const x = d3.scaleTime().range([0, width]);
-    const y = d3.scaleLinear().range([height, 0]);
-    x.domain(d3.extent(data, (d) => d.date));
-    y.domain([0, d3.max(data, (d) => d.value)]);
+    // Omit any data not present in the z-domain.
+    const I = d3.range(X.length).filter((i) => zDomain.has(Z[i]));
+
+    // Construct scales and axes.
+    const xScale = xType(xDomain, xRange);
+    const yScale = yType(yDomain, yRange);
+    const xAxis = d3.axisBottom(xScale).ticks(width / 80).tickSizeOuter(0);
+    const yAxis = d3.axisLeft(yScale).ticks(height / 60, yFormat);
+
+    // Compute titles.
+    const T = title === undefined ? Z : title === null ? null : d3.map(data, title);
+
+    // Construct a line generator.
+    const line = d3.line()
+      .defined((i) => D[i])
+      .curve(curve)
+      .x((i) => xScale(X[i]))
+      .y((i) => yScale(Y[i]));
+
+    const svg = d3.create('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('viewBox', [0, 0, width, height])
+      .attr('style', 'max-width: 100%; height: auto; height: intrinsic;')
+      .style('-webkit-tap-highlight-color', 'transparent')
+      .on('pointerenter', pointerentered)
+      .on('pointermove', pointermoved)
+      .on('pointerleave', pointerleft)
+      .on('touchstart', (event) => event.preventDefault());
+
+    // An optional Voronoi display (for fun).
+    if (voronoi) {
+      svg.append('path')
+        .attr('fill', 'none')
+        .attr('stroke', '#ccc')
+        .attr('d', d3.Delaunay
+          .from(I, (i) => xScale(X[i]), (i) => yScale(Y[i]))
+          .voronoi([0, 0, width, height])
+          .render());
+    }
+
     svg.append('g')
-      .attr('transform', `translate(0, ${height})`)
-      .call(d3.axisBottom(x));
-    svg.append('g')
-      .call(d3.axisLeft(y));
+      .attr('transform', `translate(0,${height - marginBottom})`)
+      .call(xAxis);
 
-    // add the Line
-    const valueLine = d3.line()
-      .x((d) => x(d.date))
-      .y((d) => y(d.value));
-    svg.append('path')
-      .data([data])
-      .attr('class', 'line')
+    svg.append('g')
+      .attr('transform', `translate(${marginLeft},0)`)
+      .call(yAxis)
+      .call((g) => g.select('.domain').remove())
+      .call(voronoi ? () => {} : (g) => g.selectAll('.tick line').clone()
+        .attr('x2', width - marginLeft - marginRight)
+        .attr('stroke-opacity', 0.1))
+      .call((g) => g.append('text')
+        .attr('x', -marginLeft)
+        .attr('y', 10)
+        .attr('fill', 'currentColor')
+        .attr('text-anchor', 'start')
+        .text(yLabel));
+
+    const path = svg.append('g')
       .attr('fill', 'none')
-      .attr('stroke', 'steelblue')
-      .attr('stroke-width', 1.5)
-      .attr('d', valueLine);
-  };
+      .attr('stroke', typeof color === 'string' ? color : null)
+      .attr('stroke-linecap', strokeLinecap)
+      .attr('stroke-linejoin', strokeLinejoin)
+      .attr('stroke-width', strokeWidth)
+      .attr('stroke-opacity', strokeOpacity)
+      .selectAll('path')
+      .data(d3.group(I, (i) => Z[i]))
+      .join('path')
+      .style('mix-blend-mode', mixBlendMode)
+      .attr('stroke', typeof color === 'function' ? ([z]) => color(z) : null)
+      .attr('d', ([, I]) => line(I));
 
-  useEffect(() => {
-    createGraph();
-  });
+    const dot = svg.append('g')
+      .attr('display', 'none');
+
+    dot.append('circle')
+      .attr('r', 2.5);
+
+    dot.append('text')
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', 10)
+      .attr('text-anchor', 'middle')
+      .attr('y', -8);
+
+    function pointermoved(event) {
+      const [xm, ym] = d3.pointer(event);
+      const i = d3.least(I, (i) => Math.hypot(xScale(X[i]) - xm, yScale(Y[i]) - ym)); // closest point
+      path.style('stroke', ([z]) => (Z[i] === z ? null : '#ddd')).filter(([z]) => Z[i] === z).raise();
+      dot.attr('transform', `translate(${xScale(X[i])},${yScale(Y[i])})`);
+      if (T) dot.select('text').text(T[i]);
+      svg.property('value', O[i]).dispatch('input', { bubbles: true });
+    }
+
+    function pointerentered() {
+      path.style('mix-blend-mode', null).style('stroke', '#ddd');
+      dot.attr('display', null);
+    }
+
+    function pointerleft() {
+      path.style('mix-blend-mode', mixBlendMode).style('stroke', null);
+      dot.attr('display', 'none');
+      svg.node().value = null;
+      svg.dispatch('input', { bubbles: true });
+    }
+    console.log('here!');
+    console.log(Object.assign(svg.node(), { value: null }));
+    return Object.assign(svg.node(), { value: null });
+  }
+
+  // useEffect(() => {
+  //   createGraph();
+  // });
 
   if (testRuns.length > 0) {
     return (
-      <div id="chart" />
+      <div id="chart">
+        { createGraph(testRuns, {
+          x: (d) => new Date(d.completedAt),
+          y: (d) => d.responseTime,
+          z: (d) => d.regionDisplayName,
+          yLabel: '↑ Response time (ms)',
+          width: 700,
+          height: 500,
+          color: 'steelblue',
+        }) }
+      </div>
     );
   }
 }
