74342e0d8fd45931558197caec8bd640e8aa81c5
Scott Graham <scttgrhm7+public@gmail.com>
2022-07-28 17:27:39 -0700

    fix availability metric
    Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com>
    Co-authored-by: Miles Abbason <miles.abbason@gmail.com>
    Co-authored-by: Tim Dronkers <tdronkers@gmail.com>

--- src/entities/TestRuns.js
+++ src/entities/TestRuns.js
@@ -17,6 +17,7 @@ class TestRuns {
     this._runs.forEach((run) => {
       this._responseTimes.push(run.responseTime);
       if (Number(run.responseCode) > 400) {
+        console.log('run: ', run);
         this._runsWithFailedRequests += 1;
       }
       if (!run.success) {
@@ -27,7 +28,7 @@ class TestRuns {
 
   summary() {
     const runsCount = this._runs.length;
-    const availability = ((runsCount - this._runsWithFailedAssertions) * 100.0) / runsCount;
+    const availability = ((runsCount - this._runsWithFailedRequests) * 100.0) / runsCount;
     const runsWithFailedAssertions = this._runsWithFailedAssertions;
     const responseTimeP50 = this.percentileResponseTime(50);
     const responseTimeP95 = this.percentileResponseTime(95);
