aa07cf2a4cdb7b75d28c922b35214f00dbf067d3
Tim Dronkers <tdronkers@gmail.com>
2022-07-29 16:11:54 -0600

    response headers full width Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com> Co-authored-by: Miles Abbason <miles.abbason@gmail.com> Co-authored-by: Scott Graham <scttgrhm7+public@gmail.com>

--- src/components/test-run/Headers.jsx
+++ src/components/test-run/Headers.jsx
@@ -3,7 +3,7 @@ import HeaderRows from './HeaderRows';
 
 function Headers({ headers }) {
   return (
-    <table className="min-w-fit divide-y divide-gray-300">
+    <table className="min-w-full divide-y divide-gray-300">
       <thead>
         <tr>
           <th scope="col" className="py-3 px-3 text-left text-sm font-semibold text-gray-900">
